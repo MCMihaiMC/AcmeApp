73c4d8f5182462d50966c002dc1e79ed5c914d0d
mcMihaimc <muntean.mihai70@gmail.com>
2024-04-06 20:06:46 +0300

    Finalized "properties"

--- Acme.Biz/Product.cs
+++ Acme.Biz/Product.cs
@@ -6,6 +6,12 @@
         public readonly decimal MinimumPrice;
 
         #region Constructors
+        public Product()
+        {
+            // initializing auto propertties in constructor
+            this.MinimumPrice = .96m;
+            this.Category = "tools";
+        }
         #endregion
 
         #region Properties
@@ -19,10 +25,20 @@
         private string productName;
         public string ProductName
         {
-            get { var formattedValue = productName?.Trim(); 
+            get
+            {
+                var formattedValue = productName?.Trim();
                 return formattedValue;
             }
-            set { productName = value; }
+            set
+            {
+                if (value.Length < 3)
+                    ValidationMessage = "Product Name must have at least 3 characters.";
+                else if (value.Length > 20)
+                    ValidationMessage = "Product Name must have maximum 20 characters.";
+                else
+                    productName = value;
+            }
         }
 
         private string description;
@@ -42,18 +58,57 @@
         private Vendor productVendor;
         public Vendor ProductVendor
         {
-            get {
+            get
+            {
                 if (productVendor == null)
                 {
                     productVendor = new Vendor();
                 }
                 return productVendor;
-            
+
             }
             set { productVendor = value; }
         }
 
+        public string ValidationMessage { get; private set; }
+
+        // Auto properties
+        public string Category { get; set; }
+
+        // Initialized auto properties
+        public int SequenceNumber { get; set; } = 1;
+
+        // Properties used in concatenated values
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        //public string FullName
+        //{
+        //    get { return FirstName + ", " + LastName; }
+        //}
+
+        // Expression body properties
+        public string FullName => FirstName + ", " + LastName;
+        // Properties used in concatenated values
+
+        // Properties used in calculations
+        public int Quantity { get; set; }
+        public int Price { get; set; }
+
+        public long ListItemTotal
+        {
+            get { return Quantity * Price; }
+        }
+
+        // Expression body properties
+        public int ItemTotal => Quantity * Price;
+        // Properties used in calculations
+
+        //public string VendorName => ProductVendor?.CompanyName; // ToDo: define CompanyName in vendor
+
+        public string ProductCode => this.Category + "-" + this.SequenceNumber;
+
 
         #endregion
+        }
     }
-}
---
+++ Acme.BizTests/ProductTests.cs
@@ -0,0 +1,159 @@
+using Acme.Biz;
+
+namespace Acme.BizTests
+{
+    [TestClass]
+    public class ProductTests
+    {
+        //[TestMethod()]
+        //public void Product_Null()
+        //{
+        //    //Arrange
+        //    var currentProduct = new Product();
+        //    var expected = 9.99m;
+
+        //    //Act
+        //    var actual = currentProduct.MinimumPrice;
+
+        //    //Assert
+        //    Assert.AreEqual(expected, actual);
+            
+        //}
+
+        [TestMethod()]
+        public void ProductName_Format()
+        {
+            //Arrange
+            var currentProduct = new Product();
+            currentProduct.ProductName = "  Steel Hammer  ";
+
+            var expected = "Steel Hammer";
+
+            //Act
+            var actual = currentProduct.ProductName;
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void ProductName_TooShort()
+        {
+            //Arrange
+            var currentProduct = new Product();
+            currentProduct.ProductName = "aw";
+
+            string? expected = null;
+            string expectedMessage = "Product Name must have at least 3 characters.";
+
+            //Act
+            var actual = currentProduct.ProductName;
+            var actualMessage = currentProduct.ValidationMessage;
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+            Assert.AreEqual(expectedMessage, actualMessage);
+        }
+
+        [TestMethod()]
+        public void ProductName_TooLong()
+        {
+            //Arrange
+            var currentProduct = new Product();
+            currentProduct.ProductName = "Steel Bladed Hand Saw";
+
+            string? expected = null;
+            string expectedMessage = "Product Name must have maximum 20 characters.";
+
+            //Act
+            var actual = currentProduct.ProductName;
+            var actualMessage = currentProduct.ValidationMessage;
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+            Assert.AreEqual(expectedMessage, actualMessage);
+        }
+
+        [TestMethod()]
+        public void ProductName_JustRight()
+        {
+            //Arrange
+            var currentProduct = new Product();
+            currentProduct.ProductName = "Saw";
+
+            string expected = "Saw";
+            string? expectedMessage = null;
+
+            //Act
+            var actual = currentProduct.ProductName;
+            var actualMessage = currentProduct.ValidationMessage;
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+            Assert.AreEqual(expectedMessage, actualMessage);
+        }
+
+        [TestMethod()]
+        public void Category_DefaultValue()
+        {
+            //Arrange
+            var currentProduct = new Product();
+
+            var expected = "tools";
+
+            //Act
+            var actual = currentProduct.Category;
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void Category_NewValue()
+        {
+            //Arrange
+            var currentProduct = new Product();
+            currentProduct.Category = "Garden";
+
+            var expected = "Garden";
+
+            //Act
+            var actual = currentProduct.Category;
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void Sequence_DefaultValue()
+        {
+            //Arrange
+            var currentProduct = new Product();
+
+            var expected = 1;
+
+            //Act
+            var actual = currentProduct.SequenceNumber;
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+
+        [TestMethod()]
+        public void ProductCode_DefaultValue()
+        {
+            //Arrange
+            var currentProduct = new Product();
+
+            var expected = "tools-1";
+
+            //Act
+            var actual = currentProduct.ProductCode;
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+    }
+}
